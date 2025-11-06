dc4b47caef0aa123fc681444c8f8959ef96de82f
Yatinder <chhoker.yatinder123@gmail.com>
2025-11-06 14:26:06 +0530

    fix 9

--- src/components/AnnouncementBanner.jsx
+++ src/components/AnnouncementBanner.jsx
@@ -1,23 +1,25 @@
+const data = [
+  {
+    img: "/Sparkle.svg",
+    content:
+      "Exclusive Offer: Enjoy 20% Off Your First Stay – Limited Time Only! Book Now",
+  },
+];
+
 const AnnouncementBanner = () => {
   return (
     <div className="bg-[#143F35] text-primary-foreground overflow-hidden text-[#F6F4E9] ">
       <div className="animate-marquee whitespace-nowrap py-3">
         <div className="inline-flex items-center gap-8">
-          <span className="flex items-center gap-2 text-sm tracking-wider">
-            <img src="/Sparkle.svg" className="w-4 h-4" />
-            Exclusive Offer: Enjoy 20% Off Your First Stay – Limited Time Only!
-            Book Now.
-          </span>
-          <span className="flex items-center gap-2 text-sm tracking-wider">
-            <img src="/Sparkle.svg" className="w-4 h-4" />
-            Exclusive Offer: Enjoy 20% Off Your First Stay – Limited Time Only!
-            Book Now.
-          </span>
-          <span className="flex items-center gap-2 text-sm tracking-wider">
-            <img src="/Sparkle.svg" className="w-4 h-4" />
-            Exclusive Offer: Enjoy 20% Off Your First Stay – Limited Time Only!
-            Book Now.
-          </span>
+          {[...data, ...data,...data,...data].map((res, index) => (
+            <span
+              className="flex items-center gap-2 text-sm tracking-wider"
+              key={index}
+            >
+              <img src={res.img} className="w-4 h-4" />
+              {res.content}
+            </span>
+          ))}
         </div>
       </div>
     </div>
